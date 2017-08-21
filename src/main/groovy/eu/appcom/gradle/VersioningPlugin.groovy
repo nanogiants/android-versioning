eafd96efd568339e2499c8cea7fca118863b86d7
Simon van de Loo <s.vandeloo@appcom-interactive.de>
2017-08-21 12:43:38 +0200

    ACI-219 Rename tasks

--- src/main/groovy/eu/appcom/gradle/VersioningPlugin.groovy
+++ src/main/groovy/eu/appcom/gradle/VersioningPlugin.groovy
@@ -16,7 +16,7 @@ class VersioningPlugin implements Plugin<Project> {
         project.android.defaultConfig.versionCode = gitCommitCount()
         project.archivesBaseName = generateArtifactName(artifactName)
 
-        def printVersionInfo = project.tasks.create("printVersionInfo") {
+        def printVersionInfo = project.tasks.create("versioning: printVersions") {
             doLast {
                 println "Version Name: " + gitTag()
                 println "Version Code: " + gitCommitCount()

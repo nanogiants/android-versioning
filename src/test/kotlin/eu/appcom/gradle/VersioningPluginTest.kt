4827964619cf993b4c03567ba72289fd830c648a
Nils Druyen <n.druyen@appcom-interactive.de>
2019-11-22 23:41:16 +0100

    Refactor to junit 5

--- src/test/kotlin/eu/appcom/gradle/VersioningPluginTest.kt
+++ src/test/kotlin/eu/appcom/gradle/VersioningPluginTest.kt
@@ -4,105 +4,92 @@ import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.revwalk.RevCommit
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
-import org.junit.*
-import org.junit.Assert.assertTrue
-import org.junit.rules.TemporaryFolder
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
 import java.io.IOException
-
+import java.nio.file.Files
+import java.nio.file.Path
 
 class VersioningPluginTest {
 
-    companion object {
+  companion object {
 
-        @JvmField
-        @ClassRule
-        val testProjectDir = TemporaryFolder()
+    @TempDir
+    @JvmStatic
+    lateinit var tempProjectDir: Path
 
-        lateinit var settingsFile: File
-        lateinit var buildFile: File
-
-        val projectFolder: TemporaryFolder by lazy {
-            val folder = TemporaryFolder()
-            folder.create()
-            folder.newFile("build.gradle")
-            folder
-        }
-
-        private val project: Project by lazy {
-            ProjectBuilder.builder()
-                    .withProjectDir(projectFolder.root)
-                    .build()
-        }
-
-        private val plugin: VersioningExtension by lazy {
-            project.pluginManager.apply("eu.appcom.gradle.android-versioning")
-            val extension = project.extensions.getByName("versioning")
-            assertTrue(extension is VersioningExtension)
-            extension as VersioningExtension
-        }
-
-        lateinit var git: Git
-
-        @BeforeClass
-        @JvmStatic
-        fun setup() {
-            git = Git.init().setDirectory(projectFolder.root).call()
-            println("init git ${git.repository.directory} ${plugin.getVersionName()} - ${plugin.getVersionCode()}")
-
-            settingsFile = testProjectDir.newFile("settings.gradle");
-            buildFile = testProjectDir.newFile("build.gradle");
-        }
-
-        @AfterClass
-        @JvmStatic
-        fun cleanup() {
-            println("cleanup test directory")
-            projectFolder.delete()
-        }
+    private val project: Project by lazy {
+      ProjectBuilder.builder()
+        .withProjectDir(tempProjectDir.toFile())
+        .build()
     }
 
-    @Test
-    fun `commit count higher 0`() {
-        addCommit()
-        assert(0 < plugin.getVersionCode())
+    private val plugin: VersioningExtension by lazy {
+      project.pluginManager.apply("eu.appcom.gradle.android-versioning")
+      val extension = project.extensions.getByName("versioning")
+      assertTrue(extension is VersioningExtension)
+      extension as VersioningExtension
     }
 
-    @Test
-    fun `get apk names`() {
-        // TODO
+    lateinit var git: Git
+
+    @BeforeAll
+    @JvmStatic
+    fun setup() {
+      Files.createFile(tempProjectDir.resolve("build.gradle"))
+
+      git = Git.init().setDirectory(tempProjectDir.toFile()).call()
+      println("init git ${git.repository.directory} ${plugin.getVersionName()} - ${plugin.getVersionCode()}")
+    }
+
+    @AfterAll
+    @JvmStatic
+    fun cleanup() {
+      println("cleanup test directory")
+      tempProjectDir.toFile().delete()
+    }
+  }
+
+  @Test
+  fun `commit count higher 0`() {
+    addCommit()
+    assert(0 < plugin.getVersionCode())
+  }
+
+  @Test
+  fun `get apk names`() {
+    // TODO
 //        GradleRunner.create()
 //                .withProjectDir(testProjectDir.root)
 //                .withArguments("${taskName}")
 //                .build()
+  }
+
+  // Utility methods
+
+  @Throws(IOException::class)
+  private fun writeFile(destination: File, content: String) {
+    var output: BufferedWriter? = null
+    try {
+      output = BufferedWriter(FileWriter(destination))
+      output.write(content)
+    } finally {
+      output?.close()
     }
+  }
 
-    // Utility methods
+  private fun addCommit(): RevCommit {
+    File(tempProjectDir.toFile(), "build.gradle").appendText("// addition")
+    git.add().addFilepattern("build.gradle").call()
+    return git.commit().setSign(false).setMessage("addition").call()
+  }
 
-    @Throws(IOException::class)
-    private fun writeFile(destination: File, content: String) {
-        var output: BufferedWriter? = null
-        try {
-            output = BufferedWriter(FileWriter(destination))
-            output.write(content)
-        } finally {
-            output?.close()
-        }
-    }
-
-    private fun addCommit(): RevCommit {
-        File(projectFolder.root, "build.gradle").appendText("// addition")
-        git.add().addFilepattern("build.gradle").call()
-        return git.commit().setMessage("addition").call()
-    }
-
-//    private RevCommit addCommit() {
-//        new File(projectFolder.root, "build.gradle").append("// addition")
-//        git.add().addFilepattern("build.gradle").call()
-//        git.commit().setMessage("addition").call()
-//    }
 //
 //    private void addTag(String tagName) {
 //        git.tag().setName(tagName).call()
